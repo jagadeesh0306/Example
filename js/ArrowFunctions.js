9855fa3f9439a5a760c900a69951c6ae6efe0f07
jagadeesh0306 <111483611+jagadeesh0306@users.noreply.github.com>
2024-06-03 17:17:22 +0530

    Add files via upload

---
+++ js/ArrowFunctions.js
@@ -0,0 +1,12 @@
+/*var add = function(x, y){
+    var result = x+y;
+    return result;
+}
+var r1 = add(3,9);
+console.log(r1);*/
+
+// Using arrow functions.
+
+const add = (n1, n2) => n1 + n2;
+ res = add(5, 7);
+console.log(res);
---
+++ js/letAndConstant.js
@@ -0,0 +1,9 @@
+{
+     let num = 10;
+     console.log(num);
+}
+//console.log(num);
+
+const num  = 20;
+//num = 30;
+console.log(num);
